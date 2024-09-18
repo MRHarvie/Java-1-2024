c46a76f42a6828b6f792737f0939af3d90c06e74
Mitchell.Harvie <mitchell.harvie25@ed.cna.nl.ca>
2024-09-18 10:25:34 -0230

    Fun With Classes

--- src/main/java/ca/nl/ca/mitchell/harvie/objects/FunWithStudents.java
+++ src/main/java/ca/nl/ca/mitchell/harvie/objects/FunWithStudents.java
@@ -9,18 +9,25 @@ public class FunWithStudents {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Fun with Students");
-
-        Student student = new Student();
+        //Initiating a class named "student"
+        Student firstStudent = new Student();
+        Student secondStudent = new Student();
+        
         System.out.println("Please enter a Name:  ");
         String y = input.next();
-        student.setFirstName(y);
+        firstStudent.setFirstName(y);
+
+        System.out.println("Please enter a Name:  ");
+        String y2 = input.next();
+        secondStudent.setFirstName(y2);
 
         Account account = new Account();
         System.out.println("Please enter a number:  ");
         String x = input.next();
         account.setName(x);
 
-        System.out.printf("Students first name: %s\n", student.getFirstName());
+        System.out.printf("Students name: %s\n", firstStudent.getFirstName());
+        System.out.printf("Second Students name: %s\n", secondStudent.getFirstName());
         System.out.printf("Students account: %s", account.getName());
     }
 }

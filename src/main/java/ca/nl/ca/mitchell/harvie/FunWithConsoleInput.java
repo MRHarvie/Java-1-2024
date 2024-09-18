f746c5e7b133d1d699d590e14e5116f2bdd494b7
Harvie  <Mitchell.Harvie@k202domain.com>
2024-09-18 09:04:04 -0230

    Initial commit 2

---
+++ src/main/java/ca/nl/ca/mitchell/harvie/FunWithConsoleInput.java
@@ -0,0 +1,25 @@
+package ca.nl.ca.mitchell.harvie;
+
+import java.util.Scanner;
+
+/**
+ * in this example we will use simple console input from your keyboard
+ * using a Scanner object
+ */
+
+public class FunWithConsoleInput {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please enter a number: ");
+        int x = input.nextInt();
+
+        System.out.println("Please enter a second number:  ");
+        int y = input.nextInt();
+
+        int sum = x + y;
+
+        System.out.printf("\nValue input: %d", sum);
+    }
+
+}
---
+++ src/main/java/ca/nl/ca/mitchell/harvie/FunWithMathOperations.java
@@ -0,0 +1,33 @@
+package ca.nl.ca.mitchell.harvie;
+import java.util.Scanner;
+/**
+ * Having fun with the math operations
+ */
+public class FunWithMathOperations {
+    public static void main(String[] args) {
+        System.out.println("Fun with Math!");
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please enter a number:  ");
+        int x = input.nextInt();
+
+        System.out.println("Please enter a second number:  ");
+        int y = input.nextInt();
+
+        System.out.println("Please enter a third number:   ");
+        int z = input.nextInt();
+        
+        //Addition
+        System.out.printf("\nSum of %d and %d and %d is %d", x, y, z, x+y+z);
+        //Subtraction
+        System.out.printf("\nSubstrate of %d and %d is %d", x, y, x-y);
+        //Division
+        System.out.printf("\nQuotient of %d and %d is %d", x, y, x/y);
+        //Multiplication
+        System.out.printf("\nMultiple of %d and %d is %d", x, y, x*y);
+        //Remainder
+        System.out.printf("\nRemainder of %d and %d is %d", x, y, x%y);
+
+    }
+}

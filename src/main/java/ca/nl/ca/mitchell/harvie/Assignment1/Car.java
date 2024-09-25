36ea207998f03bbea7a74a302d478dfb7fedc7c6
Mitchell.Harvie <mitchell.harvie25@ed.cna.nl.ca>
2024-09-25 12:01:52 -0230

    Assignment 1

--- src/main/java/ca/nl/ca/mitchell/harvie/Assignment1/Car.java
+++ src/main/java/ca/nl/ca/mitchell/harvie/Assignment1/Car.java
@@ -24,30 +24,65 @@ public class Car {
     }
 
     /**
-     * Getters and Setters for 4 given values
+     * Getter for make.
      * @return
      */
     public String getMake() {
         return make;
     }
+
+    /**
+     * Setter for make.
+     * @param make
+     */
     public void setMake(String make) {
         this.make = make;
     }
+
+    /**
+     * Getter for model.
+     * @return
+     */
     public String getModel() {
         return model;
     }
+
+    /**
+     * Setter for model
+     * @param model
+     */
     public void setModel(String model) {
         this.model = model;
     }
+
+    /**
+     * Getter for year.
+     * @return
+     */
     public int getYear() {
         return year;
     }
+
+    /**
+     * Setter for year
+     * @param year
+     */
     public void setYear(int year) {
         this.year = year;
     }
+
+    /**
+     * Getter for value.
+     * @return
+     */
     public double getValue() {
         return value;
     }
+
+    /**
+     * Setter for value.
+     * @param value
+     */
     public void setValue(double value) {
         this.value = value;
     }
--- src/main/java/ca/nl/ca/mitchell/harvie/Assignment1/FunWithCars.java
+++ src/main/java/ca/nl/ca/mitchell/harvie/Assignment1/FunWithCars.java
@@ -14,27 +14,16 @@ public class FunWithCars {
 
         System.out.print("Enter the car's make: ");
         String make = scanner.nextLine();
-
         System.out.print("Enter the car' model: ");
         String model = scanner.nextLine();
-
         System.out.print("Enter the car's year: ");
         int year = scanner.nextInt();
-
         System.out.print("Enter the car's value: ");
         double value = scanner.nextDouble();
 
-        /**
-         * Car instance creation using "Car" class
-         */
         Car car = new Car(make, model, year, value);
 
-        /**
-         * Print out given information to console.
-         */
-        System.out.printf("Car Information:\nMake: %s\nModel: %s\nYear: %d\nValue: $%.2f%n",car.getMake(),car.getModel(),car.getYear(),car.getValue());
-
-
-
+        System.out.printf("Car Information:\nMake: %s\nModel: %s\nYear: %d\nValue: $%.2f%n"
+                , car.getMake(),car.getModel(),car.getYear(),car.getValue());
     }
 }
